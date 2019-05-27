3d794298dfbcdb7e496666cb8a3f5242932e6f5a
Akinyi <margaretaoch@gmail.com>
2019-05-27 15:38:44 +0200

    Login changes

--- src/webshop/Login.java
+++ src/webshop/Login.java
@@ -4,6 +4,6 @@ package webshop;
 
 public class Login {
 String connectionString="jdbc:mysql://localhost:3306/webshoptest2";
-String name="Akinyi";
-String password="java2msql2018";
+String name="Someone";
+String password="passwords";
 }
